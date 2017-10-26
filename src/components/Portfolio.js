4b7ecd2ab1596c807efb3f8c07451b099df42cd9
Dakota Nichols <dakotachantz@gmail.com>
2017-10-26 11:32:45 -0500

    added new item to portfolio

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -3,6 +3,7 @@ import React, { Component } from "react";
 import basicsMockup from "../images/e-commerce-site.png";
 import codeSnippetMockup from "../images/code-snippets.png";
 import songShareMockup from "../images/song-share-app.png";
+import homelessHelperMockup from "../images/homelesshelper.png";
 
 export default class Portfolio extends Component {
   render() {
@@ -12,14 +13,14 @@ export default class Portfolio extends Component {
           id="portfolio"
           className="parallax"
           style={{
-            backgroundImage: `url(${basicsMockup})`,
-            WebkitBackgroundImage: `url(${basicsMockup})`,
+            backgroundImage: `url(${homelessHelperMockup})`,
+            WebkitBackgroundImage: `url(${homelessHelperMockup})`,
             height: "100%",
             minHeight: "100vh",
             width: "100%",
             backgroundSize: "135%",
             backgroundPosition: "center",
-            backgroundColor: "#333",
+            backgroundColor: "#39444d",
             display: "flex",
             justifyContent: "flex-end",
             alignItems: "center"
@@ -40,6 +41,62 @@ export default class Portfolio extends Component {
               Portfolio
             </h2>
           </div>
+          <div
+            className="work-intro"
+            style={{
+              width: "50%",
+              backgroundColor: "rgba(57, 68, 77, 0.7)",
+              minHeight: "100vh",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              flexDirection: "column"
+            }}
+          >
+            <h3 className="work-title" style={{ color: "#fff" }}>
+              Homeless Helper
+            </h3>
+            <div className="work-descr" style={{ color: "#fff" }}>
+              Built with React, Google Maps API, Open Data API, PostgreSQL,
+              Bootstrap, and JavaScript.
+            </div>
+            <div className="workButtons">
+              <a
+                href="http://back-to-basics.surge.sh/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="btn btn-mod btn-medium"
+              >
+                Live
+              </a>
+              <a
+                href="https://github.com/dakotachantz/e-commerce-site"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="btn btn-mod btn-medium"
+              >
+                Code
+              </a>
+            </div>
+          </div>
+        </div>
+        <div
+          id="portfolio"
+          className="parallax"
+          style={{
+            backgroundImage: `url(${basicsMockup})`,
+            WebkitBackgroundImage: `url(${basicsMockup})`,
+            height: "100%",
+            minHeight: "100vh",
+            width: "100%",
+            backgroundSize: "135%",
+            backgroundPosition: "center",
+            backgroundColor: "#333",
+            display: "flex",
+            justifyContent: "flex-end",
+            alignItems: "center"
+          }}
+        >
           <div
             className="work-intro"
             style={{
